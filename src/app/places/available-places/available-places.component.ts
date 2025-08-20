d67e1ea45106e88244932ffbbcd0a6594cf3c750
Daniel <danielgibson.pe@gmail.com>
2025-08-20 13:51:25 +0200

    Fallback code, v224

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -15,17 +15,23 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef)
 
   ngOnInit() {
-    const subscription = this.httpClient.get<{places: Place[] }>('http://localhost:3000/places').pipe(
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{places: Place[] }>('http://localhost:3000/places')
+    .pipe(
       map((resData) => resData.places) //not needed
     )
     .subscribe({
       next: (places) => {
         this.places.set(places);
-
+      },
+      complete: () => {
+        this.isFetching.set(false);
       }
     });
 
